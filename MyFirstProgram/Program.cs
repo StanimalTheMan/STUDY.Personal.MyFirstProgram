4daca2c55b4b706548bc4e7fb026cbd788a06796
Stan Choi <sjc591@nyu.edu>
2023-09-03 09:14:34 -0400

    Console.WriteLine modified

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please type your name");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine($"Hello {name}.  It's {date}.  This is your math's game.  That's great that you're working on improving yourself\n");
+Console.WriteLine(@$"What game would you like to play today?  Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("---------------------------------");
+
+var gameSelected = Console.ReadLine();
+
+if (gameSelected.Trim().ToLower() == "a")
+{
+    Console.WriteLine("Addition game selected");
+}
+else if (gameSelected.Trim().ToLower() == "s")
+{
+    Console.WriteLine("Subtraction game selected");
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    Console.WriteLine("Multiplication game selected");
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    Console.WriteLine("Division game selected");
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
