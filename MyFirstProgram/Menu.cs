aa496d71ecbf8455897681f1c67247052c082a96
Stan Choi <sjc591@nyu.edu>
2023-09-12 12:08:15 -0400

    Use OOP to refactor math game

---
+++ MyFirstProgram/Menu.cs
@@ -0,0 +1,57 @@
+﻿namespace MyFirstProgram
+{
+    internal class Menu
+    {
+        GameEngine gameEngine = new();
+
+        internal void ShowMenu(string name, DateTime date)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Hello {name}.  It's {date}.  This is your math's game.  That's great that you're working on improving yourself\n");
+
+            var isGameOn = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@$"
+What game would you like to play today?  Choose from the options below:
+V - View Previous Games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+                Console.WriteLine("---------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        Helpers.GetGames();
+                        break;
+                    case "a":
+                        gameEngine.AdditionGame("Addition game");
+                        break;
+                    case "s":
+                        gameEngine.SubtractionGame("Subtraction game");
+                        break;
+                    case "m":
+                        gameEngine.MultiplicationGame("Multiplication game");
+                        break;
+                    case "d":
+                        gameEngine.DivisionGame("Division game");
+                        break;
+                    case "q":
+                        Console.WriteLine("Goodbye");
+                        isGameOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            } while (isGameOn);
+        }
+    }
+}
